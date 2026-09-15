f01be39dabea490476de7c7564dee309cfe9c3b6
Your Name <727724euec084@skcet.ac.in>
2026-09-15 23:12:54 +0530

    Connect OxyGuard to new Supabase

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,7 +1,9 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseKey = import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY;
+const supabaseUrl = "https://psyurwvdnizzatwhfhmt.supabase.co";
+
+const supabaseKey =
+  "sb_publishable_yyaAtjmFPQiMgTjYPzmsgQ_6iKzjx_Z";
 
 export const supabase = createClient(
   supabaseUrl,
